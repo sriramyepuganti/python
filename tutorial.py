8a7d9455974b61059ccc020a3b8b98ffd03f28bd
Sriram sai Yepuganti <39261521+sriramyepuganti@users.noreply.github.com>
2022-02-02 22:58:29 +0530

    feat(functions): add functions and data types

--- tutorial.py
+++ tutorial.py
@@ -172,3 +172,136 @@ def passFun():
     pass
 for i in range(1,5):
     pass
+
+# Strings
+# Updating the content of the strings is as easy as assigning it to a new string. The string object doesn't support item assignment i.e., A string can only be replaced with new string since its content cannot be partially replaced. Strings are immutable in Python.
+
+str = "HELLO WORLD"
+strDoc = """ Doc comment """
+print(strDoc)
+print(str*3)
+print("Length of string:",len(str))
+print("slice the string:",str[0:5])
+print("slice the string with steps str(start,stop,step):",str[::2])
+print("reverse the string:",str[::-1])
+print("check val is present in string:",'Hell' in str)
+# to print raw string
+print(r'C://python37/test')
+print("The string str : %s"%(str))
+print("{} and {} both are the best friend".format("sriram","sai"))
+print("{a},{b},{c}".format(a = "James", b = "Peter", c = "Ricky"))
+print("capitalize",str.capitalize())
+print("casefold",str.casefold())
+print("center the string:",str.center(20))
+print("count number of occurrence of sub string:",str.count("L"))
+print("ends with:", str.endswith("LD"))
+print("find:",str.find('L'))
+print("join:", ",".join(['1','2','3']))
+
+#Lists
+# Python lists are mutable type its mean we can modify its element after it created.
+# The lists are ordered and mutable type.
+# l1*2 => l1 l1, l1 + l1 => l1 l1, ele in l1, len(l1), cmp(l1,l2), max(l1), min(l1), list(other_data_type)
+
+list1 = [1,2,"Peter",4.50,"Ricky",5,6]  
+list2 = [1,2,5,"Peter",4.50,"Ricky",6]  
+print(list1==list2) # False
+
+print('combine the list',list1 + list2)
+newList = []
+for i in range(0,10):
+    newList.append(i) # add element
+print(newList)
+newList.remove(4) #remove element
+print("remove:", newList)
+
+
+# tuples
+# Python Tuple is used to store the sequence of immutable Python objects.
+# tuple is indexed in the same way as the lists.
+# Unlike lists, the tuple items cannot be deleted by using the del keyword as tuples are immutable
+# # t1*2 => t1 t1, t1 + t1 => t1 t1, ele in t1, len(t1), cmp(t1,t2), max(t1), min(t1), tuple(other_data_type)
+
+tupleTest = ('str')
+tupleTest1 = ('str',)
+print(type(tupleTest))
+print(type(tupleTest1))
+
+# sets
+# A Python set is the collection of the unordered items. Each element in the set must be unique, immutable, and the sets remove the duplicate elements.
+# It can contain any type of element such as integer, float, tuple etc. But mutable elements (list, dictionary, set) can't be a member of set.
+# clear,add,update, remove,discard,copy,pop,union,intersection, set(list)
+setDays = {"Monday", "Tuesday", "Wednesday", "Thursday","Sunday"} 
+setDays.add("Friday")
+setDays.update(["Friday", "Saturday"]) # it accepts iterable ele
+setDays.discard("Tuesday"); # it will not throw error if item not found
+#setDays.remove("Tuesday"); # it will throw error if item not found
+print(setDays)    
+print(type(setDays))    
+print("looping through the set elements ... ")    
+for i in setDays:    
+    print(i)
+
+# dictionary 
+# Python Dictionary is used to store the data in a key-value pair format. The dictionary is the data type in Python
+# Keys must be a single element
+# Value can be any type such as list, tuple, integer, etc.
+
+# dict({}), del dic[0], pop(), i in dict.values(), x,y in dict.items(), len(), get(), copy(), has_key(), count(), 
+
+
+# functions
+# all the changes made to the reference inside the function revert back to the original value referred by the reference.
+# Python provides the facility to pass the multiple keyword arguments which can be represented as **kwargs. It is similar as the *args but it stores the argument in the dictionary format.
+
+
+def returnNone():
+    return 
+print(returnNone())
+
+def returnSomething():
+    return 50
+    
+print(returnSomething())
+
+def change_list(list1):    
+    list1.append(20)   
+    list1.append(30)    
+    print("list inside function = ",list1)    
+    
+#defining the list    
+listForFun = [10,30,40,50]    
+    
+#calling the function     
+change_list(listForFun)  
+print("list outside function = ",listForFun)
+
+def defaultArg(a,b=2):
+    return a+b
+print(defaultArg(10))
+
+def varArgs(*var):
+    print(var)
+varArgs(1,2,3,45)
+
+def KeywordArg(p,t):
+    print(p,t)
+KeywordArg(t=100,p=10)
+
+
+lambdaExmp = lambda a:a**3
+print(lambdaExmp(5))
+
+# filter,map, reduce
+from functools import reduce
+filterExm = (1,2,3,4,5,6,7,8)
+filterOutput = filter(lambda x: x%2,filterExm )
+print(set(filterOutput))
+
+mapOutput = map(lambda x: x*2, filterExm)
+print(set(mapOutput))
+
+
+reduceOutput = reduce(lambda a,b:a+b,filterExm)
+print((reduceOutput))
+
