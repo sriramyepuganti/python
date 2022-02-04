5b0894200e01515a8797e977e5de535f7ed2d193
Sriram sai Yepuganti <39261521+sriramyepuganti@users.noreply.github.com>
2022-02-04 19:28:01 +0530

    feat(mouldes): add modules

--- tutorial.py
+++ tutorial.py
@@ -191,7 +191,7 @@ print("The string str : %s"%(str))
 print("{} and {} both are the best friend".format("sriram","sai"))
 print("{a},{b},{c}".format(a = "James", b = "Peter", c = "Ricky"))
 print("capitalize",str.capitalize())
-print("casefold",str.casefold())
+# print("casefold",str.casefold())
 print("center the string:",str.center(20))
 print("count number of occurrence of sub string:",str.count("L"))
 print("ends with:", str.endswith("LD"))
@@ -305,3 +305,222 @@ print(set(mapOutput))
 reduceOutput = reduce(lambda a,b:a+b,filterExm)
 print((reduceOutput))
 
+# input
+ip = input("Enter a value or string:")
+print(ip)
+
+# files
+# modes x-> create a new file, throws error if file name already exists, r->read, w->write, a->append, r+ ->read and write, w+ -> write and read, a+ -> append and read, rb -> read binary, wb,ab, rb+,wb+,ab+
+file = open('index.html','w')
+if file:
+    print('file created')
+else:
+    print('file creation failed')
+try:
+    file.write('<html></html>')
+finally:
+    file.close()
+
+fileAppend = open('index.html','a')
+if file:
+    try:
+        fileAppend.write('<body></body>')
+    finally:
+        file.close()
+else:
+    print('did not find the file')
+
+fileRead = open('index.html','r')
+if fileRead:
+    content = fileRead.read()
+    print(content)
+fileRead.close()
+
+with open('index.html','r') as file:
+    content = file.read()
+    print(content)
+# readline() to read each line file, 
+#readlines() to read multiple lines in list
+# tell() -> tells the pointer location
+# seek() method which enables us to modify the file pointer position externally. offset([offset,from])
+
+# modules
+
+from modules.module import sum
+print(sum(1,2))
+
+from modules import module
+print(module.sum(2,3))
+
+from modules import module as test
+print(test.sum(4,5))
+# dir(package_name) -> print all the func, variables 
+# reload(package) -> reimport the file
+
+# Execption handling
+try:
+    1/0
+except Exception:
+    print(Exception)
+else:
+    print('print if succesfully done')
+finally:
+    print('it will be closed always')
+
+# custom error
+class ErrorInCode(Exception):      
+    def __init__(self, data):      
+        self.data = data      
+    def __str__(self):      
+        return repr(self.data)      
+      
+try:
+    if(2<18):
+        raise ErrorInCode(2000)
+except ErrorInCode as ae:      
+    print("Received error:", ae.data)
+
+# time
+import time
+print(time.localtime(time.time()))
+
+# Regular expression
+# match,search, findall, split, sub
+# span(): It returns the tuple containing the starting and end position of the match.
+# string(): It returns a string passed into the function.
+# group(): The part of the string is returned where the match is found.
+import re 
+reStr = "hello world"
+print(re.match("h",reStr).span())
+
+# send email
+import smtplib    
+sender_mail = 'sender@fromdomain.com'    
+receivers_mail = ['reciever@todomain.com']    
+message = """From: From Person %s  
+To: To Person %s  
+Subject: Sending SMTP e-mail   
+This is a test e-mail message.  
+"""%(sender_mail,receivers_mail)    
+try:    
+   smtpObj = smtplib.SMTP('localhost')    
+   smtpObj.sendmail(sender_mail, receivers_mail, message)    
+   print("Successfully sent email")    
+except Exception:    
+   print("Error: unable to send email")  
+
+
+# os module
+# name, makdir(name), getcwd(), chdir(), rmdir(),
+import os
+print(os.name)
+
+# magic methods
+# __init__ :  The _init_ method is called after the instance of the class has been created but before it returned to the caller.
+# __self__ : This function computes "informal" or a nicely printable string representation of an object and must return a string object.
+# __repr__ : This function is called by the repr() built-in function to compute the "official" string representation of an object and returns a machine-readable representation of a type. The goal of the _repr_ is to be unambiguous
+# __len__ : This function should return the length of an object.
+# __call__ : We can make an object callable by adding the _call_ magic method, and it is another method that is not needed quite as often is _call_. If defined in a class, then that class can be called. But if it was a function, instance itself rather than modifying.
+
+# __del__ : Just as _init_, which is a constructor method, _del_ is like a destructor. If you have opened a file in _init _, then _del_ can close it.
+
+# __bytes__ : It offers to compute a byte-string representation of an object and should return a string object.
+
+# __ge__ : This method gets invoked when >= operator is used and returns True or False.
+# __neg__ : This function gets called for the unary operator.
+
+# __ipow__ : This function gets called on the exponents with arguments. e.g. a**=b.
+
+# __le__ : This function gets called on comparison using <= operator.
+
+# _nonzero_ : This function returns the Boolean value of the object. It gets invoked when the bool (self) function is called.
+
+
+
+ # Decorators
+ # Decorators are one of the most helpful and powerful tools of Python. These are used to modify the behavior of the function. Decorators provide the flexibility to wrap another function to expand the working of wrapped function, without permanently modifying it.
+def decor_fun(fun):
+    fun()
+
+@decor_fun 
+def decor_fun_fun():
+    print('hey')
+
+# generators
+def gen():
+    yield 1
+    yield 2
+    yield 3
+
+for each in gen():
+    print(each)
+
+# json
+# loads => result will be in python dict
+# dumps => result will be in json
+# load => load(file_pointer)
+# dump => dump(file_pointer)
+
+import json
+pyDict = { 'id': 1, 'name': 'test'}
+print(type(pyDict))
+jsObj = json.dumps(pyDict)
+print(jsObj)
+print(json.loads(jsObj))
+
+# Multi processing
+from multiprocessing import Process
+
+def multi_process():
+    print ('process started')  
+if __name__ == '__main__':  
+    p = Process(target=multi_process)  
+    p.start()  
+    p.join()
+
+# OOPS concept
+
+# Encapsulation
+class Obj:
+    def __init__(self,arg1):
+        print(arg1)
+        self.display('hey...')
+    def display(self,arg1):
+        print(arg1)
+Obj(12)
+
+# Inheritance
+# multi level, multiple is supported
+
+class subObj(Obj):
+    def __init__(self):
+        print('child')
+subObj()
+#overriding
+class Animal:  
+    def speak(self):  
+        print("speaking")  
+class Dog(Animal):  
+    def speak(self):  
+        print("Barking")  
+d = Dog()  
+d.speak()
+
+
+# mysql connection
+
+# from mysql import connector
+
+# myDb = connector.connect(
+#     host = 'localhost',
+#     user = 'root',
+#     password = 'password'
+# )
+
+# myDbCursor = myDb.cursor()
+# myDbCursor.excute('Create database test')
+# print(myDb)
+# myDbCursor.execute("SHOW DATABASES")
+
+# for x in myDbCursor:
+#   print(x)
